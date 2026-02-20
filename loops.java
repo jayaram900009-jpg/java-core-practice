a8ba2de75955d5087247bffdfe642091193c2ebe
M jayaram chari <jayaram900009@gmail.com>
2026-02-20 18:09:34 +0530

    Refactor loops and add reverse number functionality
    Refactor loop implementations and add reverse number example.

--- loops.java
+++ loops.java
@@ -1,9 +1,21 @@
-public class Q1 {
+//Level 1 – Basic Loop Practice
+//Print Numbers from 1 to 10
+public class numbers {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++) { //for loop
+            System.out.println(i);
+        }
+    }
+}
+
+
+//eamples: 2
+public class reversenumber {
     public static void main(String[] args) {
         int n = 10;
-
-        for(int i=1; i<=n; i++){
-            System.out.print(i + " ");
+        while(n >= 1) {       // while loop
+            System.out.println(n);
+            n--;
         }
     }
 }
