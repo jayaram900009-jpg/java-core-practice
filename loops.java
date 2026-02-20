7784ff24ef3a02a72be8263ebddaf27800bbe714
M jayaram chari <jayaram900009@gmail.com>
2026-02-20 18:47:55 +0530

    Add even and odd number printing classes
    Added classes to print even and odd numbers from 1 to 50.

--- loops.java
+++ loops.java
@@ -9,13 +9,35 @@ public class numbers {
 }
 
 
-//eamples: 2
+//Print Numbers from 10 to 1
 public class reversenumber {
     public static void main(String[] args) {
         int n = 10;
         while(n >= 1) {       // while loop
             System.out.println(n);
             n--;
+        }
+    }
+}
+//Print Even Numbers from 1 to 50
+public class evennumbers {
+    public static void main(String[] args) {
+        int n = 50;
+        for(int i=1;i<=50;i++) {  //forloop
+            if(i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
+//Print Odd Numbers from 1 to 50
+public class odd {
+    public static void main(String[] args) {
+        int n = 1;
+        while(n<=50) {   //while loop
+            if(n % 2 != 0) 
+            System.out.println(n);
+            n++;
         }
     }
 }
